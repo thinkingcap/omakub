426b33e3bd3c6afa6d282e07f9ecb5af4ed6c8fd
Dave McLaughlin <dave@thinkingcapstudios.com>
2024-06-12 19:19:09 +1000

    Update default dock applications.

--- install/set-dock.sh
+++ install/set-dock.sh
@@ -1,2 +1,2 @@
 # Set default pinned applications (funny installer name to ensure it runs after all app installations!)
-gsettings set org.gnome.shell favorite-apps "['google-chrome.desktop', 'code.desktop', 'WhatsApp.desktop', 'signal-desktop.desktop', 'spotify.desktop', 'typora.desktop', 'pinta_pinta.desktop', 'com.github.xournalpp.xournalpp.desktop', 'org.gnome.Settings.desktop', 'org.gnome.Nautilus.desktop']"
+gsettings set org.gnome.shell favorite-apps "['google-chrome.desktop', 'org.gnome.Terminal.desktop', 'dev.warp.Warp.desktop', 'code.desktop', 'spotify.desktop', 'telegram.desktop', 'typora.desktop', 'com.github.xournalpp.xournalpp.desktop', 'org.gnome.Settings.desktop', 'org.gnome.Nautilus.desktop']"
