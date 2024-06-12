878b72b4361fde1629f9ecefb397a2c1683a49f2
Dave McLaughlin <dave@thinkingcapstudios.com>
2024-06-12 15:02:48 +1000

    Install Warp terminal.

---
+++ install/app-warp.sh
@@ -0,0 +1,7 @@
+sudo apt-get install -y wget gpg
+wget -qO- https://releases.warp.dev/linux/keys/warp.asc | gpg --dearmor > warpdotdev.gpg
+sudo install -D -o root -g root -m 644 warpdotdev.gpg /etc/apt/keyrings/warpdotdev.gpg
+sudo sh -c 'echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/warpdotdev.gpg] https://releases.warp.dev/linux/deb stable main" > /etc/apt/sources.list.d/warpdotdev.list'
+rm warpdotdev.gpg
+sudo apt update
+sudo apt install -y warp-terminal
